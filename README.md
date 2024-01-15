62a7bac1df7b6ac53963ba07fd2ed84f0015d862
Jasper Moelker <jasper@voorhoede.nl>
2024-01-15 21:23:50 +0100

    feat: HTML preview (wip)
    prototype, should use debouncing, error handling, UI improvements, etc

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # DatoCMS Plugin: OEmbed
 
-**[DatoCMS plugin](https://www.datocms.com/marketplace/plugins) to get embed data for external content, using the OEmbed.**
+**[DatoCMS plugin](https://www.datocms.com/marketplace/plugins) to get embed data for external content, using the [OEmbed](https://oembed.com/).**
 
-This plugin supports [~300 content providers](https://oembed.com/providers.json) using the [OEmbed](https://oembed.com/) protocol. The OEmbed endpoints can't be called directly from a DatoCMS plugin due to CORS policies. This project therefore contains two packages:
+## OEmbed providers
+
+This plugin supports [~300 content providers](https://oembed.com/providers.json) (like Twitter, YouTube, and many more) using the [OEmbed](https://oembed.com/) protocol.
+
+## Packages
+
+The OEmbed endpoints can't be called directly from a DatoCMS plugin due to CORS policies. This project therefore contains two packages:
 
 - [`oembed-api`](/packages/oembed-api/): API to get OEmbed data for a given URL.
 - [`oembed-plugin`](/packages/oembed-plugin/): DatoCMS plugin using the API package, storing the OEmbed data and showing an HTML preview.
--- packages/oembed-plugin/src/entrypoints/FieldExtension.tsx
+++ packages/oembed-plugin/src/entrypoints/FieldExtension.tsx
@@ -1,4 +1,4 @@
-// import { useState, useCallback, useEffect } from 'react'
+import { useState } from 'react'
 import type { RenderFieldExtensionCtx } from 'datocms-plugin-sdk';
 import { Canvas } from 'datocms-react-ui';
 import _get from 'lodash/get';
@@ -8,21 +8,50 @@ type Props = {
   ctx: RenderFieldExtensionCtx
 }
 type OEmbedData = {
-    url: string;
+    [key: string]: any
 }
 
 export default function FieldExtension({ ctx }: Props) {
     const urlFieldPath = ctx.fieldPath.replace(new RegExp(`${ctx.field.attributes.api_key}$`), ctx.parameters.urlFieldKey as string);
     const urlFieldValue = (_get(ctx.formValues, urlFieldPath) as string || '').trim();
-    const dataFieldValue = _get(ctx.formValues, ctx.fieldPath) as OEmbedData || {};
-    console.log({ urlFieldValue, dataFieldValue });
-    if (urlFieldValue !== dataFieldValue.url) {
-        const provider = getOEmbedProvider(urlFieldValue);
-        console.log('provider?', provider);
+    // const dataFieldValue = _get(ctx.formValues, ctx.fieldPath) as OEmbedData || "{}";
+    // console.log({ urlFieldValue, dataFieldValue });
+    const provider = urlFieldValue ? getOEmbedProvider(urlFieldValue) : null;
+    const [data, setData] = useState<OEmbedData | null>(null);
+    // const [error, setError] = useState<string | null>(null);
+    if (provider) {
+        const url = new URL('https://datocms-plugin-oembed.pages.dev/api/oembed');
+        url.searchParams.set('url', urlFieldValue);
+        fetch(url.toString())
+            .then(res => res.json())
+            .then(res => {
+                if (res.data) {
+                    setData(res.data)
+                    ctx.setFieldValue(ctx.fieldPath, JSON.stringify(res.data, null, 2));
+                }
+            });
     }
+
     return (
         <Canvas ctx={ctx}>
-            <p>Field extension: { ctx.parameters.urlFieldKey as string }</p> 
+            {
+                provider && data && (<>
+                    <details open>
+                        <summary>{ provider.provider_name } preview</summary>
+                        <div
+                            dangerouslySetInnerHTML={{ __html: data.html }}
+                            style={{
+                                maxWidth: data.width ? `${data.width}px` : '100%',
+                                minHeight: data.height ? `${data.height}px` : undefined,
+                            }}
+                        />
+                    </details>
+                    <details>
+                        <summary>{ provider.provider_name } OEmbed data</summary>
+                        <pre><code>{ JSON.stringify(data, null, 2) }</code></pre>
+                    </details>
+                </>)
+            }
         </Canvas>
     )
 }
